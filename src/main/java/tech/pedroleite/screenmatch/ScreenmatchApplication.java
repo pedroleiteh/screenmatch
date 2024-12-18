a703da7f69e05de7c272fcc6671f96edfea58c12
Pedro Leite <contato.pedro77@gmail.com>
2024-12-18 16:50:55 -0300

    iniciando projeto de linha de comando com spring

---
+++ src/main/java/tech/pedroleite/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package tech.pedroleite.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro projeto Spring sem web");
+	}
+
+
+
+}
