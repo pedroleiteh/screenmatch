b7109b320937972fc1f416b7e3dfd1eb8b4d15b5
Pedro Leite <contato.pedro77@gmail.com>
2024-12-19 00:24:15 -0300

    Adiciona o Jackson-databind ao projeto e cria o dto de series

---
+++ src/main/java/tech/pedroleite/screenmatch/dtos/SerieDto.java
@@ -0,0 +1,11 @@
+package tech.pedroleite.screenmatch.dtos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SerieDto( @JsonAlias("Title") String titulo,
+                        @JsonAlias("totalSeasons") Integer totalTemporadas,
+                        @JsonAlias("imdbRating") String avaliacao) {
+    
+}
