900ed0d88036eb4b26a18240cf34b72c94f5ada7
Pedro Leite <contato.pedro77@gmail.com>
2024-12-19 01:15:23 -0300

    Cria dto para episodios

--- src/main/java/tech/pedroleite/screenmatch/ScreenmatchApplication.java
+++ src/main/java/tech/pedroleite/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tech.pedroleite.screenmatch.dtos.EpisodioDto;
 import tech.pedroleite.screenmatch.dtos.SerieDto;
 import tech.pedroleite.screenmatch.service.ConsumoApi;
 import tech.pedroleite.screenmatch.service.ConverteDados;
@@ -17,12 +18,21 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 		ConsumoApi consumoApi = new ConsumoApi();
 		ConverteDados conversor = new ConverteDados();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=suits&apikey=1d86c7af");
 		
 		SerieDto dados = conversor.obterDados(json, SerieDto.class);
 		System.out.println(dados);
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=suits&&season=1&episode=4&apikey=1d86c7af");
+
+		EpisodioDto dadosEp = conversor.obterDados(json, EpisodioDto.class);
+		System.out.println(dadosEp);
+
+
 		
 	}
 }
---
+++ src/main/java/tech/pedroleite/screenmatch/dtos/EpisodioDto.java
@@ -0,0 +1,13 @@
+package tech.pedroleite.screenmatch.dtos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodioDto(  @JsonAlias("Title") String titulo,
+                            @JsonAlias("Season") Integer temporada,
+                            @JsonAlias("Episode") Integer numero,
+                            @JsonAlias("imdbRating") String avaliacao,
+                            @JsonAlias("Released") String dataLancamento) {
+    
+}
