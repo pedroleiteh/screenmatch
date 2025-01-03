17baea36f932b49f0e6c32c52a5ea9ce208637f6
Pedro Leite <contato.pedro77@gmail.com>
2025-01-03 11:15:14 -0300

    Remove a logica do commandLineRunner da classe principal e adiciona a dependecia do spring web para começarmos a refatoracao para transformar a aplicacao em uma api

--- src/main/java/tech/pedroleite/screenmatch/ScreenmatchApplication.java
+++ src/main/java/tech/pedroleite/screenmatch/ScreenmatchApplication.java
@@ -1,27 +1,12 @@
 package tech.pedroleite.screenmatch;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import tech.pedroleite.screenmatch.repository.SerieRepository;
-import tech.pedroleite.screenmatch.ui.Ui;
-
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner{
-
-	@Autowired
-	private SerieRepository repository;
+public class ScreenmatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Ui ui = new Ui(repository);
-		ui.exibiMenu();
 	}
 }
